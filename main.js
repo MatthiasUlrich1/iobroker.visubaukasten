f77cb4a63a2c80778ee63600ba578ef2d09ec963
unknown <info@my-smart-home-support.de>
2025-07-09 21:52:18 +0200

    ...

--- main.js
+++ main.js
@@ -1,167 +1,112 @@
-"use strict";
+'use strict';
 
-/*
- * Created with @iobroker/create-adapter v2.6.5
- */
+const utils = require('@iobroker/adapter-core'); // Adapter-Baseclass
+const { exec } = require('child_process');
 
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
-const utils = require("@iobroker/adapter-core");
+// Adapter-Abhängigkeiten und Mindestversionen
+const requiredAdapters = {
+    "javascript": "9.0.7",
+    "web": "7.0.8",
+    "vis": "1.5.6",
+    "trashschedule": "3.3.0",
+    "ical": "1.16.6",
+    "alias-manager": "2.0.0",
+    "vis-jqui-mfd": "1.0.12",
+    "vis-materialdesign": "1.7.2",
+    "vis-timeandweather": "1.2.2"
+};
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
-
-class Visubaukasten extends utils.Adapter {
-
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "visubaukasten",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
-
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
-
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
-
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
-
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
-
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
-
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
-	}
-
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
+// Versionsvergleich (true, wenn v1 >= v2)
+function compareVersions(v1, v2) {
+    const a = v1.split('.').map(Number);
+    const b = v2.split('.').map(Number);
+    for (let i = 0; i < Math.max(a.length, b.length); i++) {
+        const n1 = a[i] || 0;
+        const n2 = b[i] || 0;
+        if (n1 > n2) return true;
+        if (n1 < n2) return false;
+    }
+    return true;
 }
 
-if (require.main !== module) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new Visubaukasten(options);
+class Visubaukasten extends utils.Adapter {
+    constructor(options) {
+        super({
+            ...options,
+            name: 'visubaukasten',
+        });
+
+        this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+    }
+
+    async onReady() {
+        this.log.info('Adapter visubaukasten gestartet.');
+
+        // Optional: Adapter-Option für automatische Installation
+        const autoInstall = this.config.autoInstallDependencies || false;
+
+        for (const [adapter, minVersion] of Object.entries(requiredAdapters)) {
+            try {
+                const obj = await this.getForeignObjectAsync('system.adapter.' + adapter + '.0');
+                if (!obj) {
+                    this.log.warn(`Adapter "${adapter}" ist NICHT installiert! Benötigt wird mindestens Version ${minVersion}.`);
+                    if (autoInstall) {
+                        this.installAdapter(adapter, minVersion);
+                    }
+                } else {
+                    const currentVersion = obj.common.version;
+                    if (!compareVersions(currentVersion, minVersion)) {
+                        this.log.warn(`Adapter "${adapter}" ist in Version ${currentVersion} installiert, benötigt wird mindestens ${minVersion}.`);
+                        if (autoInstall) {
+                            this.upgradeAdapter(adapter, minVersion);
+                        }
+                    } else {
+                        this.log.info(`Adapter "${adapter}" ist in ausreichender Version (${currentVersion}) installiert.`);
+                    }
+                }
+            } catch (e) {
+                this.log.error(`Fehler beim Prüfen von "${adapter}": ${e.message}`);
+            }
+        }
+
+        // Hier dein eigentlicher Adapter-Code...
+    }
+
+    installAdapter(adapter, version) {
+        this.log.info(`Starte Installation von ${adapter}@${version} ...`);
+        exec(`iobroker add ${adapter}@${version} --log-level warn`, (error, stdout, stderr) => {
+            if (error) {
+                this.log.error(`Fehler bei Installation von ${adapter}: ${error.message}`);
+            } else {
+                this.log.info(`Adapter ${adapter} wurde installiert.`);
+            }
+        });
+    }
+
+    upgradeAdapter(adapter, version) {
+        this.log.info(`Starte Upgrade von ${adapter}@${version} ...`);
+        exec(`iobroker upgrade ${adapter}@${version} --log-level warn`, (error, stdout, stderr) => {
+            if (error) {
+                this.log.error(`Fehler bei Upgrade von ${adapter}: ${error.message}`);
+            } else {
+                this.log.info(`Adapter ${adapter} wurde aktualisiert.`);
+            }
+        });
+    }
+
+    onUnload(callback) {
+        try {
+            this.log.info('Adapter visubaukasten gestoppt.');
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+}
+
+if (module.parent) {
+    module.exports = (options) => new Visubaukasten(options);
 } else {
-	// otherwise start the instance directly
-	new Visubaukasten();
+    new Visubaukasten();
 }
